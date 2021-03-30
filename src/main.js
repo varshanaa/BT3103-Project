c03185bdc280118d04d1d70d44bc2ff8a9ad6c44
tan-si-jing <65453969+tan-si-jing@users.noreply.github.com>
2021-03-31 03:11:06 +0800

    checks user auth state before mount

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./routes";
+import {firebase}   from './firebase'
+import 'bootstrap';
+import './assets/app.scss';
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+let app = '';
+
+firebase.auth().onAuthStateChanged(function(user) {
+
+  if(!app){
+    new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+    
+  }
+
+});
+
