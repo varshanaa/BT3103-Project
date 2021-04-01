fc4e70419f519c453028fea15421dd5957dfc249
tan-si-jing <65453969+tan-si-jing@users.noreply.github.com>
2021-04-02 02:12:25 +0800

    Added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,24 +1,63 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from "./routes";
-import {firebase}   from './firebase'
-import 'bootstrap';
-import './assets/app.scss';
+import Vue from "vue";
+import App from "./App.vue";
+import routes from "./routes";
+import jQuery from 'jquery';
+import {fb} from './firebase'
+import VueFirestore from 'vue-firestore'
+require('firebase/firestore')
 
-Vue.config.productionTip = false
+
+Vue.use(VueFirestore, {
+  key: 'id',         // the name of the property. Default is '.key'.
+  enumerable: true  //  whether it is enumerable or not. Default is true.
+})
+
+
+Vue.use(VueFirestore)
+
+import Vue2Filters from 'vue2-filters'
+Vue.use(Vue2Filters)
+
+window.$ = window.jQuery = jQuery;
+
+import 'popper.js';
+import 'bootstrap';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+import PortalVue from 'portal-vue'
+
+Vue.use(PortalVue)
+
+import Swal from 'sweetalert2';
+
+window.Swal = Swal;
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
+window.Toast = Toast;
+
+
+Vue.config.productionTip = false;
 
 let app = '';
 
-firebase.auth().onAuthStateChanged(function(user) {
+fb.auth().onAuthStateChanged(function() {
 
   if(!app){
     new Vue({
-      router,
-      store,
+      routes,
       render: h => h(App)
     }).$mount("#app");
     
   }
 
 });
-
