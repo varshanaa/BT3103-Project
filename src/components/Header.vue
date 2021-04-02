f91677d8b32b95b7c10ed6d88d8aeda4dc917350
tan-si-jing <65453969+tan-si-jing@users.noreply.github.com>
2021-04-02 23:17:26 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #C1D9CA;">
   <a class="navbar-brand" href="/" style="font-family:EB Garamond; margin-right: 10px;">
     <img src="../../public/YGS-logo.png" width="30" height="30" class="d-inline-block align-top" style="margin: 0px 10px;">
-    Your Green Stop
+    YOUR GREEN STOP
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
