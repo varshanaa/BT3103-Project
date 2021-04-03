56776558acf5a68c6b6cc08261c61af8531d7508
weelin26 <70046059+weelin26@users.noreply.github.com>
2021-04-03 12:19:42 +0800

    Added searchbar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input id="searchbar" placeholder="Search Products" v-on:keyup.enter="route($event)"/> 
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    route: function (event) {
+      let keyword = event.target.value;
+      this.$router
+        .push({ path: "app", query: { searchTerm: keyword } }) //link to allProductsPage
+        .then(() => location.reload());
+    },
+  },
+};
+</script>
+
+<style scoped>
+#searchbar {
+  position: absolute;
+  text-align: center;
+  top: 58px;
+  width: 300px;
+  height: 25px;
+  left: 40%;
+  font-family: "EB Garamond";
+  background: #c1d9ca;
+  border-radius: 6px;
+} 
+</style>
