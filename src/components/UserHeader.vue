89db155558e307038cddc3678774d575f176d0bb
tan-si-jing <65453969+tan-si-jing@users.noreply.github.com>
2021-04-04 02:47:38 +0800

    Update UserHeader.vue

--- src/components/UserHeader.vue
+++ src/components/UserHeader.vue
@@ -1,155 +1,106 @@
 <template>
-<div>
-  <header>
-    <button id = "signout">SIGN OUT</button>
-    <button id = "product"> PRODUCT </button> 
-    <b id="websitename">Your Green Stop</b>
-    <img id = "logo" src="https://www.flaticon.com/svg/vstatic/svg/892/892890.svg?token=exp=1617044888~hmac=66b5bcb898e1d36f484dee081ee57d81">
-    <input id = "searchbar" placeholder="Search">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <button id="userprofile"><i class='fa fa-user-circle-o' style='font-size:28px'></i></button>
-    <button id="searchicon"><i class="fa fa-search" style='font-size:24px'></i></button>
-    <p id="userdata">Welcome {{user}}!</p>
-   <button id="likedicon"><i class="fa fa-heart" style='font-size:26px'></i></button>
-    <button id="carticon"><i class="fa fa-shopping-cart" style="font-size:28px"></i></button>
-  </header>
-</div>
+	<div class="homepage">
+		<div class="newspaper-content">
+			<div class="newspaper-feed">
+			</div>
+			<div class="stores-title"> Partner Stores </div>
+			<div class = "filters"> 
+				<div class= "categories-filter"> Categories </div>
+			</div>
+			<ul>
+				<li v-for="shop in shopsList" v-bind:key="shop.name">
+					<img v-bind:src="shop.img_url">
+					<h2> {{shop.name}} </h2>
+				</li>
+			</ul>
+		</div>
+		<Footer/>
+	</div> 
 </template>
 
 <script>
-import {fb} from "../firebase.js"
+import {database} from "../firebase.js"
+import Footer from './FooterComponent.vue'
 
-export default {
-  data() {
-    return {
-      user : "Sarah",
-    }
-  },
-  methods:{
-    printProfile: function(){
-      const user = fb.auth().currentUser;
-      console.log(user);
-    }
-  },
-}
+export default({
+	data(){
+		return{
+			shopsList:[],
+		}
+	},
+
+	components: {
+		Footer
+	},
+
+	methods: {
+		fetchItems: function(){
+			database.collection("companies").get().then((querySnapShot) =>{
+				let shop = {};
+				querySnapShot.forEach(doc => {
+					shop = doc.data();
+					this.shopsList.push(shop);
+				})
+			})
+		}
+	},
+
+	created(){
+		this.fetchItems()
+	}
+
+})
 </script>
+
+
 <style scoped>
-
- header{
-	background: #EDF6F9;
-	position: sticky;
-  min-height: 120px;
- }
-
-#product{
-  position: absolute;
-    width: 95px;
-    height: 40px;
-    left: 20px;
-    top: 60px;
-    border-radius: 10px;
-    font-family: 'EB Garamond';
-    font-size: 13px;
-    color: #D8E2DC;
-    background: #688A75;
-}
-#signout {
-    position: absolute;
-    width: 95px;
-    height: 40px;
-    right:20px;
-    top: 60px;
-    border-radius: 10px;
-    font-family: 'EB Garamond';
-    font-size: 13px;
-    color: #D8E2DC;
-    background: #688A75;
+.homepage {
+	width: 100%;
+    margin: 0px;
+    box-sizing: border-box;
 }
 
-#websitename {
-  position: absolute;
-  font-weight: 500;
-  font-size: 20px;
-	left: 44%;
-  text-transform: uppercase;
-  color: rgba(36,88,57,1);
-  font-family: EB Garamond;
-  margin-top: 10px;
+.newspaper-feed {
+	width: 75%;
+	height: 500px;
+    border: 2px solid rgba(104, 138, 117, 1);
+    display: inline-block;
+    margin: 50px;
 }
 
-
-#logo {
-    position: absolute;
-    margin-top: 10px;
-    margin-left:190px;
-    width: 30px;
-		left: 44%;
-    height: 30px; 
+.stores-title {
+	font-size: 40px;
+	font-family: 'EB Garamond';
 }
 
-#userdata {
-    position: absolute;
-    top: 5px;
-    left: 80%;
-    height: 30px;
+.filters {
+	margin-top: 40px;
+	margin-bottom: 20px;
+	font-size: 30px;
 }
 
-#userprofile {
-   color:#688A75;
-   margin-top:14px;
-   margin-left: 88%;
-   background-color: Transparent;
-   background-repeat: no-repeat;
-   border: none;
+ul {
+    display: inline-flex;
+	flex-wrap: wrap;
+	gap:60px;
+    list-style-type: none;
+	justify-content: center;
+    padding-left: 0px;
 }
 
-
-#likedicon {
-  position: absolute;
-  top: 16px;
-  left: 92%;
-  color:#688A75;
-  background-color: Transparent;
-  background-repeat:no-repeat;
-  border: none;
-}
-
-#carticon {
-  position: absolute;
-  top: 14px;
-  left: 96%;
-  background-color: Transparent;
-  background-repeat:no-repeat;
-  border: none;
-  color:#688A75;
-}
-
-#searchbar {
-    position: absolute;
+li {
     text-align: center;
-    top: 58px;
-    width: 300px;
-    height: 25px;
-    left: 40%;
-    font-family: 'EB Garamond';
-    background: #C1D9CA;
-    border-radius: 6px;
 }
 
-#search{
-   position: absolute;
-    margin-top: 58px;
-    margin-left:190px;
-    width: 30px;
-    height: 30px; 
+img {
+    width: 380px;
+    height: 300px;
 }
 
-#searchicon{
-  margin-left:62%;
-  margin-top:15px;
-  background-color: Transparent;
-  background-repeat:no-repeat;
-  border: none;
-  color:#688A75;
+h2{
+	font-family: 'EB Garamond';
+	font-size: 20px;
+	color: rgba(0, 86, 94, 1);
+	padding:10px;
 }
- </style>
+</style>
