20c89bbc134e9b0ee47f39ae21fa7dd7f405564a
weelin26 <70046059+weelin26@users.noreply.github.com>
2021-04-08 18:26:04 +0800

    Searchbar for logged-in users

---
+++ src/components/UserSearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input id="searchbar" placeholder="Search Products" v-on:keyup.enter="route($event)"/> 
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    route: function (event) {
+      let keyword = event.target.value;
+      this.$router
+        .push({ path: "/user/app", query: { searchTerm: keyword } }) //link to AllProductsPage.vue
+        .then(() => location.reload());
+    },
+  },
+};
+</script>
+
+<style scoped>
+#searchbar {
+  border-radius: 6px;
+}
+</style>
